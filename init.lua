d80578caab1daadb121a6b51b82d786643a0bd2e
amitshky <amitshkymail@gmail.com>
2024-09-21 22:10:40 +0545

    mouse update

--- init.lua
+++ init.lua
@@ -277,6 +277,7 @@ require('lazy').setup({
 -- options
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.mousemodel = "extend"
 vim.o.cursorline = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
